c4fa75dc803711037ff2c8c91506824590d57f7d
EmilyCV <emily_cvasconcelos@hotmail.com>
2022-05-17 14:20:45 -0300

    a simple nose test case

---
+++ book/code/chapter04/test/test_module01.py
@@ -0,0 +1,15 @@
+import collections
+
+collections.Callable = collections.abc.Callable
+
+"""
+    Execute com: 
+        nosetests .\book\code\chapter04\test\test_module01.py 
+    
+    nosetests:    
+        - Usar o comando nosetests é a maneira mais simples de executar os módulos de teste. 
+        Devido à simplicidade e conveniência do estilo de codificação e invocação.
+"""
+def test_case01():
+    assert 'aaa'.upper() == 'AAA'
+
